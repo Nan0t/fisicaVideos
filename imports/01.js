b520bc825420b6ac13d6b6cac2768885f589c57f
Hernan <hsolis@est.frba.utn.edu.ar>
2020-04-29 07:08:25 -0300

    arreglo variables

--- imports/01.js
+++ imports/01.js
@@ -1,5 +1,5 @@
-	var title1 = "Espejo y esquema de rayos"
-	var videos1 = [ 
+	var title = "Espejo y esquema de rayos"
+	var videos = [ 
 		['Parte 1',['E9lueFPgLDw']],
 		['Parte 2',['lwy2K9o_sQY']],
 		['Parte 3',['6r8Hu7b5D7g']]
--- imports/02.js
+++ imports/02.js
@@ -1,5 +1,5 @@
-	var title2 = "Trabajo: Ejercicios"
-	var videos2 = [ 
+	var title = "Trabajo: Ejercicios"
+	var videos = [ 
 		['Ejercicio 86', ['kzANt1gnPsU']],
 		['Ejercicios 91 y 97',['l9jX015uKFU']],
 		['Ejercicio 95', ['dX2Efx0TQXk','NowkOXrP_rQ','RRDRycIXnrI','98gVYXeLJ38','ize9E8qmTXs'] ]
