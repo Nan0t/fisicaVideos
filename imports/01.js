5abc45ed0e24114a5a4f513050019078fa548e3b
Hernan <hsolis@est.frba.utn.edu.ar>
2020-04-29 06:48:55 -0300

    prueba js

---
+++ imports/01.js
@@ -0,0 +1,6 @@
+	var title1 = "Espejo y esquema de rayos"
+	var videos1 = [ 
+		['Parte 1',['E9lueFPgLDw']],
+		['Parte 2',['lwy2K9o_sQY']],
+		['Parte 3',['6r8Hu7b5D7g']]
+	];
---
+++ imports/02.js
@@ -0,0 +1,6 @@
+	var title2 = "Trabajo: Ejercicios"
+	var videos2 = [ 
+		['Ejercicio 86', ['kzANt1gnPsU']],
+		['Ejercicios 91 y 97',['l9jX015uKFU']],
+		['Ejercicio 95', ['dX2Efx0TQXk','NowkOXrP_rQ','RRDRycIXnrI','98gVYXeLJ38','ize9E8qmTXs'] ]
+	];
